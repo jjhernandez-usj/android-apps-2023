456f59e64ce4e56411221096876b2bebcae3268a
TomasRipsky <alu.133086@usj.es>
2023-03-01 15:45:53 +0100

    Trying onClick listener

--- app/src/main/java/es/usj/jhernandez/ProfileActivity.kt
+++ app/src/main/java/es/usj/jhernandez/ProfileActivity.kt
@@ -2,10 +2,19 @@ package es.usj.jhernandez
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import es.usj.jhernandez.databinding.ActivityMainBinding
+import es.usj.jhernandez.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+    private val binding : ActivityProfileBinding by lazy {
+        ActivityProfileBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(binding.root)
+        binding.tvPhone.setOnClickListener{
+            Toast.makeText(this,"hola", Toast.LENGTH_SHORT).show()
+        }
     }
 }
