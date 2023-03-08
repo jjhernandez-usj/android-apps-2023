368be730b2f44b69143fa70e283b52142c8ae667
Anselm82 <anselm82@gmail.com>
2023-03-08 15:12:45 +0100

    feature/lifecycle: added lifecycle methods override.

---
+++ app/src/main/java/es/usj/jhernandez/ManualCreatedActivity.kt
@@ -0,0 +1,52 @@
+package es.usj.jhernandez
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import es.usj.jhernandez.databinding.ActivityManualCreatedBinding
+
+class ManualCreatedActivity : AppCompatActivity() {
+
+    private val view : ActivityManualCreatedBinding by lazy {
+        ActivityManualCreatedBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(view.root)
+        show("onCreate")
+    }
+    private fun show(method: String) {
+        Toast.makeText(this, "Method $method called!!", Toast.LENGTH_SHORT).show()
+    }
+    override fun onStart() {
+        super.onStart()
+        show("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        show("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        show("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        show("onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        show("onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        show("onDestroy")
+    }
+}
