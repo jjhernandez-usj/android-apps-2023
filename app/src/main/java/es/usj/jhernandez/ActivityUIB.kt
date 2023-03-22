b17d53f8833a7c3f4ad4a207630bf6e48a405a03
mBnc34 <104487495+mBnc34@users.noreply.github.com>
2023-03-22 16:44:23 +0100

    feature/feature/profile_activity: image and text view with name

---
+++ app/src/main/java/es/usj/jhernandez/ActivityUIB.kt
@@ -0,0 +1,11 @@
+package es.usj.jhernandez
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityUIB : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_uib)
+    }
+}
