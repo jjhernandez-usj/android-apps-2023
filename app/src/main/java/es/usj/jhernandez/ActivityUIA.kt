c33d4a6fd390bf3f5f019622c9d8669536b1b3d6
Anselm82 <anselm82@gmail.com>
2023-03-22 16:02:08 +0100

    feature/ui: enabling and disabling button implemented.

--- app/src/main/java/es/usj/jhernandez/ActivityUIA.kt
+++ app/src/main/java/es/usj/jhernandez/ActivityUIA.kt
@@ -2,10 +2,37 @@ package es.usj.jhernandez
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
+import es.usj.jhernandez.databinding.ActivityUiaBinding
 
 class ActivityUIA : AppCompatActivity() {
+
+    private val view : ActivityUiaBinding by lazy {
+        ActivityUiaBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_uia)
+        setContentView(view.root)
+        view.btnLogin.isEnabled = false
+        view.etUsername.addTextChangedListener {
+            userEmpty = it?.length == 0
+            checkBtnEnabled()
+        }
+
+        view.etPassword.addTextChangedListener {
+            passEmpty = it?.length == 0
+            checkBtnEnabled()
+        }
+
+    }
+
+    private fun checkBtnEnabled() {
+        view.btnLogin.isEnabled = areFieldCompleted()
+    }
+
+    private var userEmpty = true
+    private var passEmpty = true
+    private fun areFieldCompleted() : Boolean {
+        return !userEmpty && !passEmpty
     }
 }
