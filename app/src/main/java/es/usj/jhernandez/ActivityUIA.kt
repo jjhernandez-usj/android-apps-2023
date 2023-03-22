faf212b9fe2336250b6b18bf2d0a6b85db988ccd
Skiruu <66178759+Skiruu@users.noreply.github.com>
2023-03-22 16:14:30 +0100

    feature/ui: cheking the login

--- app/src/main/java/es/usj/jhernandez/ActivityUIA.kt
+++ app/src/main/java/es/usj/jhernandez/ActivityUIA.kt
@@ -2,6 +2,7 @@ package es.usj.jhernandez
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import es.usj.jhernandez.databinding.ActivityUiaBinding
 
@@ -24,6 +25,10 @@ class ActivityUIA : AppCompatActivity() {
             checkBtnEnabled()
         }
 
+        view.btnLogin.setOnClickListener{
+            correctLogin()
+        }
+
     }
 
     private fun checkBtnEnabled() {
@@ -34,5 +39,17 @@ class ActivityUIA : AppCompatActivity() {
     private var passEmpty = true
     private fun areFieldCompleted() : Boolean {
         return !userEmpty && !passEmpty
+    }
+
+    private fun correctLogin(){
+
+        if(view.etUsername.text.toString()==view.etPassword.text.toString()){
+
+            Toast.makeText(this, "Welcome ${view.etUsername.text}" , Toast.LENGTH_SHORT).show()
+
+        }else{
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
