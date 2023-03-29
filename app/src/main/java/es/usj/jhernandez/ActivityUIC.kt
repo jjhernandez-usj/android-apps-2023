06b3b515cb38b51bfecd9a4e616bf9625fc2400c
mBnc34 <104487495+mBnc34@users.noreply.github.com>
2023-03-29 16:41:06 +0200

    feature/feature/profile_activity: image and text view with name

---
+++ app/src/main/java/es/usj/jhernandez/ActivityUIC.kt
@@ -0,0 +1,11 @@
+package es.usj.jhernandez
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityUIC : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_uic)
+    }
+}
