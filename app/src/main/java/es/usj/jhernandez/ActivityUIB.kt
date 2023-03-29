5edf0494e409b84e8cc75bf735a9f05b18acc729
Jordan <105800909+HectorGJordan@users.noreply.github.com>
2023-03-29 16:25:19 +0200

    feature/merge Switching Layouts

--- app/src/main/java/es/usj/jhernandez/ActivityUIB.kt
+++ app/src/main/java/es/usj/jhernandez/ActivityUIB.kt
@@ -2,10 +2,46 @@ package es.usj.jhernandez
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import es.usj.jhernandez.databinding.ActivityUiaBinding
+import es.usj.jhernandez.databinding.ActivityUibBinding
 
 class ActivityUIB : AppCompatActivity() {
+
+    private val view : ActivityUibBinding by lazy {
+        ActivityUibBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_uib)
+        setContentView(view.root)
+
+        view.rbtnCitizen.setOnCheckedChangeListener { compoundButton, _ -> onRadioButtonClicked(compoundButton) }
+        view.rbtncompany.setOnCheckedChangeListener { compoundButton, _ -> onRadioButtonClicked(compoundButton) }
     }
+
+    private fun onRadioButtonClicked(view: View) {
+
+        if (view is RadioButton ) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.rbtnCitizen ->
+                    if (checked) {
+                        this.view.companyLayout.visibility = View.INVISIBLE
+                        this.view.citizenLayout.visibility = View.VISIBLE
+                    }
+                R.id.rbtncompany ->
+                    if (checked) {
+                    this.view.citizenLayout.visibility = View.INVISIBLE
+                        this.view.companyLayout.visibility = View.VISIBLE
+                    }
+            }
+        }
+    }
+
+
 }
