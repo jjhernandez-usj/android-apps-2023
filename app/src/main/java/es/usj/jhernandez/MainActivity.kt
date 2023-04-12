d83bed076e53c8b6d322b7d67369920cd770bf77
Skiruu <66178759+Skiruu@users.noreply.github.com>
2023-04-12 16:41:10 +0200

    feature/ex2_adapter

--- app/src/main/java/es/usj/jhernandez/MainActivity.kt
+++ app/src/main/java/es/usj/jhernandez/MainActivity.kt
@@ -2,8 +2,11 @@ package es.usj.jhernandez
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 import es.usj.jhernandez.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +22,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(view.root)
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
         view.listView.adapter = adapter
+        view.listView.onItemClickListener = object: AdapterView.OnItemClickListener{
+            override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                Toast.makeText(this@MainActivity, " ", Toast.LENGTH_SHORT).show()
+            }
 
+        }
     }
 }
