fd53950e2841604a9a980724001753554678e60e
adnen <adnenkhazri@gmail.com>
2023-04-19 15:47:35 +0200

    ex3-modify data from adapter

--- app/src/main/java/es/usj/jhernandez/MainActivity.kt
+++ app/src/main/java/es/usj/jhernandez/MainActivity.kt
@@ -2,6 +2,7 @@ package es.usj.jhernandez
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -16,20 +17,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val items = listOf("item1", "item2", "item3")
+        val items = mutableListOf("item1", "item2", "item3")
         super.onCreate(savedInstanceState)
 
         setContentView(view.root)
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
         view.listView.adapter = adapter
         view.listView.onItemClickListener = object: AdapterView.OnItemClickListener{
-            override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+            override fun onItemClick(adapterView: AdapterView<*>?, view: View?, position: Int, p3: Long) {
 
-                val selectedItem = adapter.getItem(p2)
+                val selectedItem = adapter.getItem(position)
                 Toast.makeText(this@MainActivity, selectedItem+" clicked", Toast.LENGTH_SHORT).show()
-                //Toast.makeText(this@MainActivity, " ", Toast.LENGTH_SHORT).show()
+                items.remove(selectedItem)
+                Log.e("list:", items.toString())
+                adapter.notifyDataSetChanged()
             }
 
+
+        }
+        view.addbtn.setOnClickListener {
+            items.add("item ${Math.random()}")
+            adapter.notifyDataSetChanged()
         }
     }
 }
